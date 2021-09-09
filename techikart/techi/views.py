e3331ffa4dd908cf454815d300a054a53771766d
Sunil-Kumar-P <sunilkpg23@gmail.com>
2021-09-09 17:23:30 +0530

    updated

---
+++ techikart/techi/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+def product(request):
+    return render(request, 'product.html')
